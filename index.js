1d673bc31a1045a21fb9e13cb2e6d6f0e8ade114
ShreeyansB <sbahadkar@rocketmail.com>
2021-11-25 21:41:59 +0530

    :art: Reformat code

--- index.js
+++ index.js
@@ -78,4 +78,4 @@ const interval = setInterval(function ping() {
   });
 }, 5000);
 
-server.listen(PORT, () => console.log("Listening on port 3000"));
+server.listen(PORT, () => console.log("Listening on port:", PORT));
