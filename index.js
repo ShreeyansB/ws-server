b8f515d5ff8b4ffc9d494074b03756a760db6173
ShreeyansB <sbahadkar@rocketmail.com>
2021-11-25 17:27:31 +0530

    :necktie: Reworked disconnect detection logic and message passing

--- index.js
+++ index.js
@@ -2,50 +2,76 @@ const WebSocket = require("ws");
 const express = require("express");
 const app = express();
 const server = require("http").createServer(app);
+const url = require("url");
 
 const PORT = process.env.PORT || 3000;
 
 const wss = new WebSocket.Server({ server: server });
 app.get("/", (req, res) => res.send("Temst."));
 
-var tm;
-
-function ping(client) {
-  tm = setTimeout(function () {
-    console.log(`[-] ${client} Disconnected`);
-    wss.emit("customClose", client);
-  }, 5000);
-}
-
-function pong(client) {
-  clearInterval(tm);
-  // console.log("[!] Cleared timeout");
-  ping(client);
-}
+var myClients = [];
 
 wss.on("connection", function connection(ws, req) {
-  ping();
+  var queryData = url.parse(req.url, true).query;
+  myClients.push({
+    id: queryData.id,
+    wsoc: ws,
+    isAlive: true,
+  });
+
   console.log(`[+] ${req.socket.remoteAddress} Connected`);
-  ws.on("message", function incoming(message) {
-    if (message == "__ping__") {
-      console.log(`[!] Ping Receieved from ${req.socket.remoteAddress}`);
-      pong(req.socket.remoteAddress);
-    } else {
-      wss.clients.forEach(function each(client) {
-        if (client.readyState === WebSocket.OPEN) {
-          client.send(message, { binary: false });
-        }
-      });
+
+  wss.clients.forEach(function each(client) {
+    if (client.readyState === WebSocket.OPEN) {
+      message = {
+        type: "alert",
+        msg: `${queryData.id} has Connected.`,
+      };
+      client.send(JSON.stringify(message), { binary: false });
     }
+  });
+
+  ws.on("pong", () => {
+    let x = myClients.find((o) => o.wsoc === ws);
+    x.isAlive = true;
+  });
+
+  ws.on("message", function incoming(message) {
+    console.log(`[!] Message Receieved from ${req.socket.remoteAddress}`);
+    msg = JSON.parse(message);
+    console.log(queryData);
+    msg = { ...msg, time: new Date().toISOString() };
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify(msg), { binary: false });
+      }
+    });
   });
 });
 
 wss.addListener("customClose", function (m) {
   wss.clients.forEach(function each(client) {
     if (client.readyState === WebSocket.OPEN) {
-      client.send(`${m} has Disconnected.`, { binary: false });
+      message = {
+        type: "alert",
+        msg: `${m} has Disconnected.`,
+      };
+      client.send(JSON.stringify(message), { binary: false });
     }
   });
 });
+
+const interval = setInterval(function ping() {
+  myClients.forEach((clnt, index) => {
+    if (clnt.isAlive === false) {
+      console.log("[-]", clnt.id, "has Disconnected.");
+      wss.emit("customClose", clnt.id);
+      clnt.wsoc.terminate();
+      myClients.splice(index, 1);
+    }
+    clnt.isAlive = false;
+    clnt.wsoc.ping();
+  });
+}, 5000);
 
 server.listen(PORT, () => console.log("Listening on port 3000"));
